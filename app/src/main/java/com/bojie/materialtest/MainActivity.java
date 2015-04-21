66f5c61613a7fdf12b3a18d893d814f2e7151bee
Bojie Jiang <jiangbojie@gmail.com>
2015-04-21 15:39:19 -0700

    Adding a Toolbar

--- app/src/main/java/com/bojie/materialtest/MainActivity.java
+++ app/src/main/java/com/bojie/materialtest/MainActivity.java
@@ -1,17 +1,22 @@
 package com.bojie.materialtest;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
