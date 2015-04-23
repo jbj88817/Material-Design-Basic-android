166221cb08f12c3c73f5c59d92adc5aa23c6c22b
Bojie Jiang <jiangbojie@gmail.com>
2015-04-23 13:32:45 -0700

    10.Types of Navigation Drawers

--- app/src/main/java/com/bojie/materialtest/NavigationDrawerFragment.java
+++ app/src/main/java/com/bojie/materialtest/NavigationDrawerFragment.java
@@ -52,7 +52,7 @@ public class NavigationDrawerFragment extends Fragment {
         }
     }
 
-    public void setUp(int fragmentId, DrawerLayout drawerLayout, Toolbar toolbar) {
+    public void setUp(int fragmentId, DrawerLayout drawerLayout, final Toolbar toolbar) {
 
         containerView = getActivity().findViewById(fragmentId);
 
@@ -73,6 +73,14 @@ public class NavigationDrawerFragment extends Fragment {
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
                 getActivity().invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                if (slideOffset < 0.6){
+                    toolbar.setAlpha(1 - slideOffset);
+                }
+
             }
         };
 
