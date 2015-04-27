25ae3b5d17809cf6c40867803b7581c3e6590b1e
Bojie Jiang <jiangbojie@gmail.com>
2015-04-26 22:54:52 -0700

    15.Setup ViewHolder

--- app/src/main/java/com/bojie/materialtest/RecycleAdapter.java
+++ app/src/main/java/com/bojie/materialtest/RecycleAdapter.java
@@ -1,21 +1,40 @@
 package com.bojie.materialtest;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by bojiejiang on 4/25/15.
  */
 public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHolder> {
+
+    private LayoutInflater inflater;
+    List<Information> data = Collections.emptyList();
+
+    public RecycleAdapter(Context context, List<Information> data) {
+        inflater = LayoutInflater.from(context);
+        this.data = data;
+    }
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return null;
+        View view = inflater.inflate(R.layout.custom_row, viewGroup, false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder myViewHolder, int i) {
-
+        Information current = data.get(i);
+        myViewHolder.tv_title.setText(current.title);
+        myViewHolder.iv_icon.setImageResource(current.iconId);
     }
 
 
@@ -25,9 +44,15 @@ public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.MyViewHo
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView iv_icon;
+        TextView tv_title;
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            tv_title = (TextView) itemView.findViewById(R.id.listText);
+            iv_icon = (ImageView) itemView.findViewById(R.id.listIcon);
         }
+
+
     }
 }
