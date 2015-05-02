35bb158a51510f6f77a6c7c5931c78ab0513df40
Bojie Jiang <jiangbojie@gmail.com>
2015-05-02 00:07:50 -0700

    26.Make Tabs with SlidingTabLayout

--- app/src/main/java/com/bojie/materialtest/MainActivity.java
+++ app/src/main/java/com/bojie/materialtest/MainActivity.java
@@ -2,16 +2,29 @@ package com.bojie.materialtest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bojie.materialtest.util.SlidingTabLayout;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar mToolbar;
+    private ViewPager mViewPager;
+    private SlidingTabLayout mSlidingTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,12 @@ public class MainActivity extends ActionBarActivity {
         drawerFragment.setUp(R.id.fragment_navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout),
                 mToolbar);
+
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        mSlidingTabLayout = (SlidingTabLayout) findViewById(R.id.tabs);
+
+        mSlidingTabLayout.setViewPager(mViewPager);
     }
 
     @Override
@@ -52,5 +71,56 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class MyPagerAdapter extends FragmentPagerAdapter {
+
+        String[] tabs;
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+            tabs = getResources().getStringArray(R.array.tabs);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            MyFragment myFragment = MyFragment.getInstance(i);
+            return myFragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+    }
+
+    public static class MyFragment extends Fragment {
+
+        private TextView mTextView;
+
+        public static MyFragment getInstance(int position) {
+            MyFragment myFragment = new MyFragment();
+            Bundle args = new Bundle();
+            args.putInt("position", position);
+            myFragment.setArguments(args);
+            return myFragment;
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View layout = inflater.inflate(R.layout.fragment_my, container, false);
+            mTextView = (TextView) layout.findViewById(R.id.position);
+            Bundle bundle = getArguments();
+            if (bundle != null) {
+                mTextView.setText("The Page Slected is " + bundle.getInt("position"));
+            }
+            return layout;
+        }
     }
 }
