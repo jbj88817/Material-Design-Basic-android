8c9335ec1974dadaea0356082bdd6ad3f7331789
Bojie Jiang <jiangbojie@gmail.com>
2015-05-10 10:43:12 -0700

    37.Parse JSON data

--- app/src/main/java/com/bojie/materialtest/fragments/BoxOfficeFragment.java
+++ app/src/main/java/com/bojie/materialtest/fragments/BoxOfficeFragment.java
@@ -17,8 +17,33 @@ import com.bojie.materialtest.R;
 import com.bojie.materialtest.logging.L;
 import com.bojie.materialtest.material.MyApplication;
 import com.bojie.materialtest.network.VolleySingleton;
+import com.bojie.materialtest.pojo.Movie;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_AUDIENCE_SCORE;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_ID;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_MOVIES;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_POSTERS;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_RATINGS;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_RELEASE_DATES;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_SYNOPSIS;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_THEATER;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_THUMBNAIL;
+import static com.bojie.materialtest.extras.Keys.EndpointBoxOffice.KEY_TITLE;
+import static com.bojie.materialtest.extras.UriEndpoints.URL_BOX_OFFICE;
+import static com.bojie.materialtest.extras.UriEndpoints.URL_CHAR_AMEPERSAND;
+import static com.bojie.materialtest.extras.UriEndpoints.URL_CHAR_QUESTION;
+import static com.bojie.materialtest.extras.UriEndpoints.URL_PARAM_API_KEY;
+import static com.bojie.materialtest.extras.UriEndpoints.URL_PARAM_LIMIT;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,8 +51,6 @@ import org.json.JSONObject;
  * create an instance of this fragment.
  */
 public class BoxOfficeFragment extends Fragment {
-
-    public static final String URL_ROTTEN_TOMATOES_BOX_OFFICE = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json";
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -40,6 +63,8 @@ public class BoxOfficeFragment extends Fragment {
     private VolleySingleton mVolleySingleton;
     private ImageLoader mImageLoader;
     private RequestQueue mRequestQueue;
+    private ArrayList<Movie> mMoviesList = new ArrayList<>();
+    private DateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
 
     /**
@@ -61,8 +86,13 @@ public class BoxOfficeFragment extends Fragment {
     }
 
     public static String getRequestUrl(int limit) {
-        return URL_ROTTEN_TOMATOES_BOX_OFFICE + "?apikey="
-                + MyApplication.API_KEY_ROTTEN_TOMATOES + "&limit" + limit;
+
+        return URL_BOX_OFFICE
+                + URL_CHAR_QUESTION
+                + URL_PARAM_API_KEY + MyApplication.API_KEY_ROTTEN_TOMATOES
+                + URL_CHAR_AMEPERSAND
+                + URL_PARAM_LIMIT + limit;
+
     }
 
     public BoxOfficeFragment() {
@@ -79,13 +109,17 @@ public class BoxOfficeFragment extends Fragment {
 
         mVolleySingleton = VolleySingleton.getInstance();
         mRequestQueue = mVolleySingleton.getRequestQueue();
+        sendJSONRequest();
+    }
+
+    private void sendJSONRequest() {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
                 getRequestUrl(10),
-                (String)null,
+                (String) null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        L.t(getActivity(), response.toString());
+                        parseJSONResponse(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -94,6 +128,68 @@ public class BoxOfficeFragment extends Fragment {
             }
         });
         mRequestQueue.add(request);
+    }
+
+    private void parseJSONResponse(JSONObject response) {
+        if (response == null || response.length() == 0) {
+            return;
+        }
+
+        try {
+            JSONArray arrayMovies = response.getJSONArray(KEY_MOVIES);
+            for (int i = 0; i < arrayMovies.length(); i++) {
+                JSONObject currentMovie = arrayMovies.getJSONObject(i);
+
+                // Basic
+                long id = currentMovie.getLong(KEY_ID);
+                String title = currentMovie.getString(KEY_TITLE);
+
+                // Release Date
+                JSONObject objectReleaseDate = currentMovie.getJSONObject(KEY_RELEASE_DATES);
+                String releaseDate = null;
+                if (objectReleaseDate.has(KEY_THEATER)) {
+                    releaseDate = objectReleaseDate.getString(KEY_THEATER);
+                } else {
+                    releaseDate = "NA";
+                }
+
+                // Audience Score
+                JSONObject objectRatings = currentMovie.getJSONObject(KEY_RATINGS);
+                int audienceScore = -1;
+                if (objectRatings.has(KEY_AUDIENCE_SCORE)) {
+                    audienceScore = objectRatings.getInt(KEY_AUDIENCE_SCORE);
+                }
+
+                // Synopsis
+                String synopsis = currentMovie.getString(KEY_SYNOPSIS);
+
+                // Poster image
+                JSONObject objectPosters = currentMovie.getJSONObject(KEY_POSTERS);
+                String urlThumbnail = null;
+                if (objectPosters.has(KEY_THUMBNAIL)) {
+                    urlThumbnail = objectPosters.getString(KEY_THUMBNAIL);
+                }
+
+                // Set to movie object
+                Movie movie = new Movie();
+                movie.setId(id);
+                movie.setTitle(title);
+                Date date = mDateFormat.parse(releaseDate);
+                movie.setReleaseDateTheater(date);
+
+                // Add to movieList
+                mMoviesList.add(movie);
+
+            }
+
+            L.T(getActivity(), mMoviesList.toString());
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
--- app/src/main/java/com/bojie/materialtest/logging/L.java
+++ app/src/main/java/com/bojie/materialtest/logging/L.java
@@ -16,4 +16,8 @@ public class L {
     public static void t(Context context, String message) {
         Toast.makeText(context, message + "", Toast.LENGTH_SHORT).show();
     }
+
+    public static void T(Context context, String message) {
+        Toast.makeText(context, message + "", Toast.LENGTH_LONG).show();
+    }
 }
