aa8899e0b59cc82f54a304b8928581981f1f3586
Bojie Jiang <jiangbojie@gmail.com>
2015-05-25 12:13:03 -0700

    54.Transition between two activities

--- app/src/main/java/com/bojie/materialtest/activities/ActivityA.java
+++ app/src/main/java/com/bojie/materialtest/activities/ActivityA.java
@@ -1,9 +1,13 @@
 package com.bojie.materialtest.activities;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.transition.TransitionManager;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,7 +31,11 @@ public class ActivityA extends ActionBarActivity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
 //        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         super.onCreate(savedInstanceState);
-
+        if (Build.VERSION.SDK_INT >= 21) {
+            TransitionInflater inflater = TransitionInflater.from(this);
+            Transition transition = inflater.inflateTransition(R.transition.transition_a);
+            getWindow().setExitTransition(transition);
+        }
         setContentView(R.layout.activity_a);
         mRoot = (ViewGroup) findViewById(R.id.container_a);
 
@@ -66,17 +74,22 @@ public class ActivityA extends ActionBarActivity implements View.OnClickListener
     @SuppressLint("NewApi")
     @Override
     public void onClick(View v) {
-        TransitionManager.beginDelayedTransition(mRoot);
-        toggleHeight(mButton1, mButton2, mButton3, mButton4);
+
+        ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(this, null);
+        startActivity(new Intent(this, ActivityB.class), activityOptionsCompat.toBundle());
+
+
+//        TransitionManager.beginDelayedTransition(mRoot);
+//        toggleHeight(mButton1, mButton2, mButton3, mButton4);
     }
 
-    public void toggleHeight(View... views) {
-        for (View current : views) {
-            ViewGroup.LayoutParams params = current.getLayoutParams();
-            params.height = 100;
-            params.width = 50;
-            current.setLayoutParams(params);
-        }
-    }
+//    public void toggleHeight(View... views) {
+//        for (View current : views) {
+//            ViewGroup.LayoutParams params = current.getLayoutParams();
+//            params.height = 100;
+//            params.width = 50;
+//            current.setLayoutParams(params);
+//        }
+//    }
 }
 
